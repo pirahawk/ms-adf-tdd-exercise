57e54b2531b516dc151677a0e63f223591c888f0
pirahawk <pirahawk@users.noreply.github.com>
2023-01-30 20:42:43 +0000

    Setup required Nuget packages and sample scenario

---
+++ Ms.Tdd.Adf/ClassLibrary1/Features/AzureDataFactoryScenarios.feature
@@ -0,0 +1,9 @@
+﻿Feature: AzureDataFactoryScenarios
+
+I want to write some Specflow Tests to Test Azure Data Factory
+
+@tag1
+Scenario: I am writing a Test
+	Given I am writing a Test
+	#When [action]
+	#Then [outcome]
---
+++ Ms.Tdd.Adf/ClassLibrary1/Steps/TestSteps.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using TechTalk.SpecFlow;
+
+namespace Ms.Tdd.Adf.Tests.Specs.Steps
+{
+    [Binding]
+    public class TestSteps
+    {
+        [Given(@"I am writing a Test")]
+        public void GivenIAmWritingATest()
+        {
+            var test = 1;
+            test.Should().Be(1);
+        }
+    }
+}
