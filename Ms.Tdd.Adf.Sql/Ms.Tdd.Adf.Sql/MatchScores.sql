ca63587a465d9ca7071e9f4b13ee654a2efb497c
pirahawk <pirahawk@users.noreply.github.com>
2023-03-17 00:10:56 +0000

    Added SQL Data Project

---
+++ Ms.Tdd.Adf.Sql/Ms.Tdd.Adf.Sql/MatchScores.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[MatchScores]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Home] NVARCHAR(100) NOT NULL, 
+    [Away] NCHAR(100) NOT NULL, 
+    [ScoreHome] INT NULL, 
+    [ScoreAway] INT NULL
+)
